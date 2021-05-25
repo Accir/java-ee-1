e2293ed9674bf0e4cb53957a845a299cbf83670c
Accir <acciras@gmail.com>
2021-05-25 20:27:44 +0300

    Fix seed bug

--- src/main/resources/META-INF/import.sql
+++ src/main/resources/META-INF/import.sql
@@ -1,11 +1,11 @@
-insert into LAKE (ID, NAME, SIZE) values (1, 'Galve', 3.61);
-insert into LAKE (ID, NAME, SIZE) values (2, 'Sartai', 13.32);
-insert into LAKE (ID, NAME, SIZE) values (3, 'Tauragnas', 5.13);
-insert into LAKE (ID, NAME, SIZE) values (4, 'Asveja', 9.78);
+insert into LAKE (ID, NAME, SIZE, OPT_LOCK_VERSION) values (1, 'Galve', 3.61, 0);
+insert into LAKE (ID, NAME, SIZE, OPT_LOCK_VERSION) values (2, 'Sartai', 13.32, 0);
+insert into LAKE (ID, NAME, SIZE, OPT_LOCK_VERSION) values (3, 'Tauragnas', 5.13, 0);
+insert into LAKE (ID, NAME, SIZE, OPT_LOCK_VERSION) values (4, 'Asveja', 9.78, 0);
 
-insert into FISH (ID, LENGTH, FISH_SIZE, NAME, LAKE_ID) values (1, 12.5, 10.5, 'Eserys', 1);
-insert into FISH (ID, LENGTH, FISH_SIZE, NAME, LAKE_ID) values (2, 5.71, 11.13, 'Lynas', 2);
-insert into FISH (ID, LENGTH, FISH_SIZE, NAME, LAKE_ID) values (3, 6.47, 9.71, 'Menke', 1);
-insert into FISH (ID, LENGTH, FISH_SIZE, NAME, LAKE_ID) values (4, 8.15, 11.6, 'Kuoja', 3);
-insert into FISH (ID, LENGTH, FISH_SIZE, NAME, LAKE_ID) values (5, 8.96, 12.84, 'Raude', 4);
-insert into FISH (ID, LENGTH, FISH_SIZE, NAME, LAKE_ID) values (6, 12.75, 14.15, 'Lydeka', 3);
+insert into FISH (ID, LENGTH, FISH_SIZE, NAME, OPT_LOCK_VERSION, LAKE_ID) values (1, 12.5, 10.5, 'Eserys', 0, 1);
+insert into FISH (ID, LENGTH, FISH_SIZE, NAME, OPT_LOCK_VERSION, LAKE_ID) values (2, 5.71, 11.13, 'Lynas', 0, 2);
+insert into FISH (ID, LENGTH, FISH_SIZE, NAME, OPT_LOCK_VERSION, LAKE_ID) values (3, 6.47, 9.71, 'Menke', 0, 1);
+insert into FISH (ID, LENGTH, FISH_SIZE, NAME, OPT_LOCK_VERSION, LAKE_ID) values (4, 8.15, 11.6, 'Kuoja', 0, 3);
+insert into FISH (ID, LENGTH, FISH_SIZE, NAME, OPT_LOCK_VERSION, LAKE_ID) values (5, 8.96, 12.84, 'Raude', 0, 4);
+insert into FISH (ID, LENGTH, FISH_SIZE, NAME, OPT_LOCK_VERSION, LAKE_ID) values (6, 12.75, 14.15, 'Lydeka', 0, 3);
